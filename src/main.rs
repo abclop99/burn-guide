fabb28e507f794249b2716e0fb2388a37658d094
Aaron Li <aaron.li@genvira.com>
2024-12-05 11:10:55 -0500

    ✨ (training): Add and run training function
    Only tested on a weak GPU so far.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use crate::model::ModelConfig;
-use burn::backend::Wgpu;
+use burn::{
+    backend::{wgpu::WgpuDevice, Autodiff, Wgpu},
+    optim::AdamConfig,
+};
 
 mod data;
 mod model;
 mod training;
 
+use crate::{model::ModelConfig, training::TrainingConfig};
+
 fn main() {
     type MyBackend = Wgpu<f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBackend>;
 
-    let device = Default::default();
-    let model = ModelConfig::new(10, 512).init::<MyBackend>(&device);
-
-    println!("{}", model)
+    let device = WgpuDevice::default();
+    let artifact_dir = "/tmp/guide";
+    training::train::<MyAutodiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device.clone(),
+    );
 }
