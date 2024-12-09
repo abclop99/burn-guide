250e07271fdf1440bebbafde0e87ab697c9a7db6
Aaron Li <a1li@ucsd.edu>
2024-12-09 12:08:06 -0800

    🎨 (backend): Select a backend with priority
    If multiple backend features are selected, only one will actually be
    selected. There is now a default backend feature (ndarray).

--- crates/backend/src/lib.rs
+++ crates/backend/src/lib.rs
@@ -1,85 +1,73 @@
 //! Select a backend
 
-// Ensure a backend is enabled
-#[cfg(not(any(
-    feature = "ndarray",
-    feature = "ndarray-blas-accelerate",
-    feature = "ndarray-blas-netlib",
-    feature = "ndarray-blas-openblas",
-    feature = "tch-cpu",
-    feature = "tch-gpu",
-    feature = "wgpu",
-)))]
-compile_error!("None of the backend features were enabled. Exactly one must be enabled.");
-
+use cfg_if::cfg_if;
 // Re-export the specified backend
 pub use specified_backend::*;
 
-/// The NDArray backend
-#[cfg(any(
-    feature = "ndarray",
-    feature = "ndarray-blas-netlib",
-    feature = "ndarray-blas-openblas",
-    feature = "ndarray-blas-accelerate",
-))]
-mod specified_backend {
+// Select a Burn backend
+cfg_if! {
+    if #[cfg(feature = "wgpu")] {
+        mod specified_backend {
 
-    use burn::backend::ndarray::{NdArray, NdArrayDevice};
+            use burn::backend::{wgpu::WgpuDevice, Wgpu};
 
-    /// The backend to use.
-    pub type Backend = NdArray<f32, i32>;
+            /// The backend to use.
+            pub type Backend = Wgpu<f32, i32>;
 
-    /// Select a device
-    pub fn get_device() -> NdArrayDevice {
-        NdArrayDevice::Cpu
-    }
-}
-
-/// The Torch-CPU backend
-#[cfg(feature = "tch-cpu")]
-mod specified_backend {
-
-    use burn::backend::libtorch::{LibTorch, LibTorchDevice};
-
-    /// The backend to use.
-    pub type Backend = LibTorch;
-
-    /// Select a device
-    pub fn get_device() -> LibTorchDevice {
-        LibTorchDevice::Cpu
-    }
-}
-
-/// The Torch-GPU backend
-#[cfg(feature = "tch-gpu")]
-mod specified_backend {
-
-    use burn::backend::libtorch::{LibTorch, LibTorchDevice};
-
-    /// The backend to use.
-    pub type Backend = LibTorch<f32, i8>;
-
-    /// Select a device
-    pub fn get_device() -> LibTorchDevice {
-        if cfg!(target_os = "macos") {
-            LibTorchDevice::Mps
-        } else {
-            LibTorchDevice::Cuda(0)
+            /// Select a device
+            pub fn get_device() -> WgpuDevice {
+                WgpuDevice::default()
+            }
         }
-    }
-}
+    } else if #[cfg(feature = "tch-gpu")] {
+        mod specified_backend {
 
-/// The WGPU backend
-#[cfg(feature = "wgpu")]
-mod specified_backend {
+            use burn::backend::libtorch::{LibTorch, LibTorchDevice};
 
-    use burn::backend::{wgpu::WgpuDevice, Wgpu};
+            /// The backend to use.
+            pub type Backend = LibTorch<f32, i8>;
 
-    /// The backend to use.
-    pub type Backend = Wgpu<f32, i32>;
+            /// Select a device
+            pub fn get_device() -> LibTorchDevice {
+                if cfg!(target_os = "macos") {
+                    LibTorchDevice::Mps
+                } else {
+                    LibTorchDevice::Cuda(0)
+                }
+            }
+        }
+    } else if #[cfg(feature = "tch-cpu")] {
+        mod specified_backend {
 
-    /// Select a device
-    pub fn get_device() -> WgpuDevice {
-        WgpuDevice::default()
+            use burn::backend::libtorch::{LibTorch, LibTorchDevice};
+
+            /// The backend to use.
+            pub type Backend = LibTorch;
+
+            /// Select a device
+            pub fn get_device() -> LibTorchDevice {
+                LibTorchDevice::Cpu
+            }
+        }
+    } else if #[cfg(any(
+        feature = "ndarray",
+        feature = "ndarray-blas-netlib",
+        feature = "ndarray-blas-openblas",
+        feature = "ndarray-blas-accelerate",
+    ))] {
+        mod specified_backend {
+
+            use burn::backend::ndarray::{NdArray, NdArrayDevice};
+
+            /// The backend to use.
+            pub type Backend = NdArray<f32, i32>;
+
+            /// Select a device
+            pub fn get_device() -> NdArrayDevice {
+                NdArrayDevice::Cpu
+            }
+        }
+    } else {
+        compile_error!("None of the backend features were enabled. Exactly one must be enabled.");
     }
 }
